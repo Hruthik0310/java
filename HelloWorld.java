1d9e811992a0743d4cfbdf8b6abe570c811fc8ae
Hruthik <Hruthik@18.com>
2021-04-27 11:44:33 +0530

    itsdone

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("we all, rohit jayaprakash and hruthik");
+      System.out.println("we all, rohit jay and hruthik");
    }
 }
